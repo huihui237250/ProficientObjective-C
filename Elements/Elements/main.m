227b73de753ed414c99830ec666327fa900eb9ff
huihui237250 <huihui237250@126.com>
2017-01-15 16:43:20 +0800

    第一个工程

---
+++ Elements/Elements/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Elements
+//
+//  Created by glimmerlu on 2017/1/15.
+//  Copyright © 2017年 huihui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
